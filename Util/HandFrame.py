9e74e2ee921423a7121484eaf91ccd9fd2243ed0
Veirya <giovanni.oliver@gmail.com>
2024-08-24 18:42:15 -0500

    V0.2 -- Both the App and each Hand are now implemented as classes. Hands can be saved and loaded.

---
+++ Util/HandFrame.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 2 15:19:45 2024
+
+@author: Giovanni "Veirya" Oliver
+
+Class to represent a frame for a particular mahjong hand. The handData keys
+in init also serves as the template for json data for a given hand.
+"""
+import tkinter as tk
+import re
+
+class HandFrame:
+    def __init__(self, root, handData, images, bg):
+        # Load the Basics
+        self.images = images    # Tile PNGs
+        self.bg = bg            # BG Color
+        self.frame = tk.Frame(root, bg=bg)  # Own tkinter frame
+        self.rawHand = handData["hand"]     # MPSZ notation string for hand
+        self.hand = self._read_hand(self.rawHand)   # Labels for hand
+        self.rawDora = handData["dora"]     # MPSZ notation string for doras
+        self.dora = self._read_hand(self.rawDora)   # Labels for doras
+        self.shanten = handData["shanten"]  # Shanten of hand
+        self.accepts = handData["accepts"]  # Tiles that reduce shanten 
+        self.yaku = handData["yaku"]    # Main Yaku intended for hand
+        self.furiten = handData["furiten"]  # In Furiten?
+        self.won = handData["won"]  # Did you win the hand?
+        self.rtn = handData["rtn"]  # Ron/Tsumo/Neither
+        self.left = handData["left"]    # How many tiles left in the wall
+        self.where = handData["where"]  # Where was what you needed (tenpai)
+        # TODO: Probably want this eventually
+        #self.start = handData["start"]
+        
+        # Then do arrangements
+        self._arrange_hand()
+        
+    def get_frame(self):
+        return self.frame
+    
+    '''
+    Back out the hand's data for JSON packaging
+    '''
+    def get_data(self):
+        data = {}
+        data["hand"] = self.rawHand
+        data["dora"] = self.rawDora
+        data["shanten"] = self.shanten
+        data["accepts"] = self.accepts
+        data["yaku"] = self.yaku
+        data["furiten"] = self.furiten
+        data["won"] = self.won
+        data["rtn"] = self.won
+        data["left"] = self.left
+        data["where"] = self.where
+        # TODO: If this gets implemented
+        #data["start"] = self.start
+        
+        return data
+    # end def
+    
+    '''
+    Read a MPSZ notation string and return a list of Labels with the respective
+    tile images ordered as listed in the string. Called tiles should be
+    separated by a space, and face-down tiles from Kan as 'b1'.
+    Input: String in MPSZ notation
+    Output: List of tkinter laberls
+    '''
+    def _read_hand(self, input):
+        res = []
+        matches = re.findall(r'[mpszb][0-9]*| ', input)
+        for mtch in matches:
+            # Spacer for called tiles
+            if mtch == ' ':
+                label = tk.Label(self.frame, width=5)
+                label.configure(background=self.bg)
+                res.append(label)
+            else:
+                for d in mtch[1:]:
+                    # Assign the label's image according to tile
+                    img = self.images[mtch[0]][d]
+                    label = tk.Label(self.frame, image=img)
+                    label.configure(background=self.bg)
+                    res.append(label)
+                # end for
+            # end if
+        # end for
+        return res
+    # end def
+    
+    # Arrange the parts of the hand in the frame
+    def _arrange_hand(self):
+        # Arrange the labels according to position in list
+        for i, label in enumerate(self.hand):
+            label.grid(row=0, column = i, columnspan = 1)
+        # end for
+    # end def
+# end class
--- mahjongtracker.py
+++ mahjongtracker.py
@@ -10,10 +10,11 @@ Pythong tool to help keep track of various mahjong hands I've had and how I
 lost/won the hand.
 """
 
+import json
+import traceback
 import tkinter as tk
 from tilePngs import gen_img_table
-import re
-
+from Util.HandFrame import HandFrame
 '''
 Core of the app, requiring a base tkinter window/root to use
 '''
@@ -27,53 +28,45 @@ class MahjongTracker:
         self.root.minsize(1900, 1080)
         # Import tile images at 1/X scale
         self.TILE_IMAGES = gen_img_table(10)
-    # end def
+        self.loadedHands = []
 
     def get_root(self):
         return self.root
     
     '''
-    Read a mahjong hand represented by an MPSZ notation string and returns
-    a list of Labels with the respective tile image ordered as listed in the
-    string. Called tiles should be separated by a space, and face-down tiles
-    from Kan as 'b1'.
-    Input: String in MPSZ notation
-    Output: List of tkinter laberls
+    Loads previously saved hand data and creates frames for it.
     '''
-    def _read_hand(self, input):
-        res = []
-        matches = re.findall(r'[mpszb][0-9]*| ', input)
-        for mtch in matches:
-            # Spacer for called tiles
-            if mtch == ' ':
-                label = tk.Label(self.root, width=5)
-                label.configure(background = 'gray66')
-                res.append(label)
-            else:
-                for d in mtch[1:]:
-                    # Assign the label's image according to tile
-                    img = self.TILE_IMAGES[mtch[0]][d]
-                    label = tk.Label(self.root, image = img)
-                    label.configure(background = 'gray66')
-                    res.append(label)
-                # end for
-            # end if
-        # end for
-        return res
+    def _load_hands(self):
+        try:
+            with open("Data/saved_hands.json", 'r') as f:
+                data = json.load(f)
+            self.loadedHands = [HandFrame(self.root, hand, self.TILE_IMAGES,
+                                      self.MAIN_BG) for hand in data]
+        except FileNotFoundError:
+            print("No save file was found. Skipping Load.")
+    # end def
+    
+    '''
+    Save all loaded hands as JSON list. Overwrites the current save.
+    '''
+    def _save_hands(self):
+        with open("Data/saved_hands.json", 'w') as f:
+            handData = [hand.get_data() for hand in self.loadedHands]
+            json.dump(handData, f, indent=4)
+        # end with
     # end def
     
     '''
     Function scripting the current feature under test.
     '''
+    # TODO: Remove when system is more interactable
     def current_test(self):
-        # Read in a test input and display it
-        test = 'm123p406s55789 z666'
-        handLabels = self._read_hand(test)
-
-        # Arrange the labels according to position in list
-        for i, label in enumerate(handLabels):
-            label.grid(row=1, column = i, columnspan = 1)
-        # end for
+        # Load hands from the current data
+        self._load_hands()
+        #self._save_hands()
+        for i, hand in enumerate(self.loadedHands):
+            hand.get_frame().grid(row=i, column=0, sticky='ew')
+        root.grid_columnconfigure(0, weight=1)
     #end def
 #end class
     
@@ -82,9 +75,10 @@ if __name__ == "__main__":
         root = tk.Tk()
         app = MahjongTracker(root)
         app.current_test()
-        app.get_root().mainloop()
-    except Exception as e:
-        print(e)
+        root.mainloop()
+    except Exception:
+        # This T/E makes errors while running in the IDE much more tolerable
+        print(traceback.format_exc())
         root.destroy()
 #end if
 
